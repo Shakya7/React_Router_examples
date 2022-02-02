c75ce86a3dd4374c6417d326cb99cd50e5e748f2
Shakya7 <shakyasarkar72@gmail.com>
2022-02-02 20:54:53 +0530

    index_routes, useLocation, useNavigate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import {Routes, Route, Link, useParams} from "react-router-dom";
+import {Routes, Route, Link, useParams, Navigate, Outlet, NavLink, useNavigate, useLocation} from "react-router-dom";
+import axios from "axios";
 
 function Home() 
 {
@@ -32,34 +34,119 @@ function About() {
   );
 }
 
-function Page(){
+function Anime() {
+  return (
+    <React.Fragment>
+      <main>
+        <h2>Anime Section</h2>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Outlet/>
+    </React.Fragment>
+  );
+}
 
-  const obj1=useParams();
-  console.log(obj1);
+function Movies(){
+  const [movieName, setMovieName]=useState("");
+  const navigate=useNavigate();
+  
   return(
     <div>
-      <p>{`Page ${obj1.id}`}</p>
+      <p>This is the Movies Section</p>
+      <input type="text" onChange={(e)=>{
+        setMovieName(e.target.value);
+      }}/>
+      <NavLink style={({isActive})=>{
+        return{
+          color: isActive?"pink":"yellow"
+        }
+      }} to={`/movies/${movieName}`}><div>{movieName}</div></NavLink>
+      <NavLink to={`/movies/test`}><div>test</div></NavLink>
+      <Link to={`/movies/${movieName}`}><button onClick={async(e)=>{
+        console.log("Movie searched is :",movieName);
+        /*navigate(`/movies/${movieName}`,{state:{
+          abc:3,
+          xyz:10
+        }});*/
+        
+        //const result=await axios.post
+      }}>Search</button>
+      </Link>
+      <div style={{width:"40vw", height:"50vh", backgroundColor:"red"}}>
+        <Outlet/> 
+      </div>
+    </div>
+  )
+}
+function MovieDetail(){
+  const {movieName}=useParams();
+  //const location=useLocation();
+  const navigation =useNavigate();
+
+  return(
+    <div>
+      <h4>The movie is : {movieName}</h4>
+      {/*<h5>{`The state of it is ${location.state.abc}`}</h5>*/}
+      <button onClick={(e)=>{
+        navigation("/dashboard",{
+          state:{
+            message1:"This is the dashboard screen",
+            message2:"Redirected from the MovieData page"
+          }
+        })
+      }}>Move to Dashboard</button>
+
+    </div>
+  )
+}
+function Dashboard(){
+
+  const location=useLocation();
+  return(
+    <div>
+      <h1>{location.state.message1}</h1>
+      <h2>{location.state.message2}</h2>
+      <p>Hello</p>
+      <Link to="/movies">Movies</Link>
     </div>
   )
 }
 
 function App() {
-  let input=1;
+  let input=1;           //it'll only output 1, use useState here since let value is not changing
   return (
 
     <div className="App">
       <h1>WELCOME TO THE ROUTER</h1>
-      <nav style={{width:"100vw", height:"20vh", backgroundColor:"yellow"}}>
+      <nav style={{width:"90vw", height:"20vh", backgroundColor:"yellow"}}>
         <Link to="/">Home</Link>
+        <br/>
         <Link to="/about">About</Link>
-        <Link onClick={(e)=>{
-          input=prompt("Enter the page #");
-        }} to={`/pages/${input}`}>Page #</Link>
+        <br/>
+        <Link to="/movies">Movies</Link>
+        <br/>
+        <Link to="/animes">Animes</Link>
       </nav>
       <Routes>
-        <Route exact path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>}/>
-        <Route path="/pages/:id" element={<Page/>}/>
+        <Route path="/movies" element={<Movies/>}>
+          <Route path="*" element={<div>ERROR</div>}/>
+          <Route path=":movieName" element={<MovieDetail/>}/>
+        </Route> 
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/animes" element={<Anime/>}>
+          <Route index element={<div>
+            <div>
+              This is the anime data section
+            </div>
+          </div>}/>
+          <Route/>
+        </Route>
+
+        {/*<Route path="*" element={<div>ERROR</div>}/>*/}
       </Routes>
     </div>
   );
