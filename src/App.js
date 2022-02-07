62df7974bf0ac46c4ae5c88f954e2e917a598bc6
Shakya7 <shakyasarkar72@gmail.com>
2022-02-07 23:02:27 +0530

    Link filtering example done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
 
 import './App.css';
-import {Link,Outlet,Routes, Route} from "react-router-dom";
+import {Link,Outlet,Routes, Route,useParams,useSearchParams} from "react-router-dom";
+import { snArr } from './data';
+
 
 
 function Home(){
   return(
     <h2>
-      Home
+      HomePage
     </h2>
+  )
+}
+function SneakerPage(){
+  return(
+    <div>
+      <h2>This is the sneaker section</h2>
+      <Outlet/>
+    </div>
+  )
+}
+function All(){
+  const {sneakers} =snArr;
+  return(
+    <div>
+      <h2>
+        All sneakers
+      </h2>
+      <div style={{padding:"30px"}}>
+        <div style={{borderRadius:"20px",backgroundColor:"lightseagreen"}}>Search results: {sneakers.length}</div>
+        <ul style={{borderRadius:"20px",backgroundColor:"lightblue",display:'grid',gridGap:10,gridTemplateColumns:"1fr 1fr 1fr", listStyle:"none", padding:"5vmax"}}>
+        {
+          sneakers.map((e,i)=><li style={{borderRadius:"20px",backgroundColor:"red", width:"25vmax", height:"30vmin"}} key={i}>{e.name}</li>)
+        }
+        </ul>
+      </div>
+    </div>
+
   )
 
 }
-function About(){
-  return(
-    <h2>
-      About
-    </h2>
-  )
 
-}
-function Dashboard(){
+function Card(){
+  const params=useParams();
+  console.log(params);
+  const {sneakers} =snArr;
+  const ar=sneakers.filter(e=>e.brand_N===params.brand);
+  console.log(ar);
+  //console.log(sneakers);
   return(
-    <h2>
-      Dashboard
-    </h2>
+    <div>
+      <h2>{params.brand} sneakers</h2>
+      <div style={{padding:"30px"}}>
+        <div style={{borderRadius:"20px",backgroundColor:"lightseagreen"}}>Search results: {ar.length}</div>
+        <ul style={{borderRadius:"20px",backgroundColor:"lightblue",display:'grid',gridGap:10,gridTemplateColumns:"1fr 1fr 1fr", listStyle:"none", padding:"5vmax"}}>
+        {
+          ar.map((e,i)=><li style={{borderRadius:"20px",backgroundColor:"red", width:"25vmax", height:"30vmin"}} key={i}>{e.name}</li>)
+        }
+        </ul>
+      </div>
+    </div>
   )
 }
+
 function Error(){
   return (
     <div>
@@ -38,22 +75,26 @@ function Error(){
 function App() {
   return (
     <div className="App">
-      <h1>Basic Example 1</h1>
+      <h1>Custom Filter Link Example</h1>
       <br/>
-      <br/>
-      <p>This example demonstrates some of the core features of React Router including nested Route, Outlets, Links, and using a "*" route (aka "splat route") to render a "not found" page when someone visits an unrecognized URL.</p>
+      <p>This example demonstrates how to create a "filter link" like one that is commonly used to filter a list of products on an e-commerce website. The component is a custom Link that knows whether or not it is currently "active" by what is in the URL query string.</p>
+      <h4>Filter by brand</h4>
       <div style={{display:"flex", flexDirection:"column"}}>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/dashboard"}>Dashboard</Link>
-        <Link to={"/about"}>About</Link>
-        <Link to={"/error"}>Nothing here</Link>
+        <Link to="/">Home</Link>
+        <Link to={"/sneakers"}>All</Link>
+        <Link to={"/sneakers/nike"}>Nike</Link>
+        <Link to={"/sneakers/adidas"}>Adidas</Link>
+        <Link to={"/sneakers/puma"}>Puma</Link>
+        <Link to={"/error"}>Error Link</Link>
       </div>
       <hr/>          
       {/*Here we dont have to include the Outlet component*/}
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path="/sneakers" element={<SneakerPage/>}>
+          <Route index element={<All/>}/>
+          <Route path=":brand" element={<Card/>}/>
+        </Route>  
         <Route path="*" element={<Error/>}/>
       </Routes>
     </div>
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const snArr={"sneakers":[
+    {"name":"Nike A1",
+     "price":350,
+     "brand_N":"nike",
+     "color":"yellow"
+    },
+    {   "name":"Adidas Roar",
+        "price":500,
+        "brand_N":"adidas",
+        "color":"red"
+    },
+    {"name":"Nike Moderator",
+        "price":500,
+        "brand_N":"nike",
+        "color":"white"
+    },
+    {"name":"Nike B1",
+    "price":1000,
+    "brand_N":"nike",
+    "color":"maroon"
+    },
+    {   "name":"Puma A1",
+        "price":400,
+        "brand_N":"puma",
+        "color":"white"
+    },
+    {"name":"Puma Monster",
+        "price":950,
+        "brand_N":"puma",
+        "color":"green"
+       },
+       {"name":"Adidas Vector",
+        "price":650,
+        "brand_N":"adidas",
+        "color":"black"
+       },
+       {"name":"Puma A3",
+        "price":350,
+        "brand_N":"puma",
+        "color":"yellow"
+       },
+       {"name":"Nike Forza",
+        "price":3000,
+        "brand_N":"nike",
+        "color":"red-white"
+       },
+       {"name":"Nike Avendetor",
+        "price":2300,
+        "brand_N":"nike",
+        "color":"white"
+       }
+    ]
+}
