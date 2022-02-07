7813a108342761b912d66921f21919f6bb397fae
Shakya7 <shakyasarkar72@gmail.com>
2022-02-07 18:42:44 +0530

    example 1 completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Link,Outlet,Routes, Route} from "react-router-dom";
+
+
+function Home(){
+  return(
+    <h2>
+      Home
+    </h2>
+  )
+
+}
+function About(){
+  return(
+    <h2>
+      About
+    </h2>
+  )
+
+}
+function Dashboard(){
+  return(
+    <h2>
+      Dashboard
+    </h2>
+  )
+}
+function Error(){
+  return (
+    <div>
+      <h2>Nothing to see here</h2>
+      <Link to="/">Go to homepage</Link>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basic Example 1</h1>
+      <br/>
+      <br/>
+      <p>This example demonstrates some of the core features of React Router including nested Route, Outlets, Links, and using a "*" route (aka "splat route") to render a "not found" page when someone visits an unrecognized URL.</p>
+      <div style={{display:"flex", flexDirection:"column"}}>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/dashboard"}>Dashboard</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/error"}>Nothing here</Link>
+      </div>
+      <hr/>          
+      {/*Here we dont have to include the Outlet component*/}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
     </div>
   );
 }
